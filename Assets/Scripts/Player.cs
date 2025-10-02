3b8d0e7f6a355582eddcd720ed2495ede2af6437
SuperStik <daviswood290@gmail.com>
2025-10-02 11:46:22 -0700

    Add mouse look

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+    public Camera cam;
+    private InputAction look;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start() {
+        look = InputSystem.actions.FindAction("Look");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 delta = look.ReadValue<Vector2>();
+        cam.transform.eulerAngles += new Vector3(-delta.y, delta.x);
     }
 }
