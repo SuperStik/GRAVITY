cc51e7e3451c5368880fc717e47b1e746cd94354
SuperStik <daviswood290@gmail.com>
2025-10-02 16:46:27 -0700

    Add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,14 +7,25 @@ using Numerics = System.Numerics;
 
 public class Player : MonoBehaviour {
     public Camera cam;
+    
     private InputAction look;
 	private InputAction pause;
+    private InputAction move;
+    private InputAction jump;
+    
+    private Rigidbody phys;
+    private Collider collide;
 
     private Numerics.Vector2 pitchyaw;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         look = InputSystem.actions.FindAction("Look");
         pause = InputSystem.actions.FindAction("Pause");
+        move = InputSystem.actions.FindAction("Move");
+        jump = InputSystem.actions.FindAction("Jump");
+        
+        phys = GetComponent<Rigidbody>();
+        collide = GetComponent<Collider>();
         
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -26,13 +37,32 @@ public class Player : MonoBehaviour {
         pitchyaw += new Numerics.Vector2(-delta.y, delta.x);
         // fancy C# switch statement
         pitchyaw.X = pitchyaw.X switch {
-            >= 90.0f => 90.0f,
-            <= -90.0f => -90.0f,
+            > 90.0f => 90.0f,
+            < -90.0f => -90.0f,
             _ => pitchyaw.X
         };
 
         cam.transform.eulerAngles = new Vector3(pitchyaw.X, pitchyaw.Y);
+
+        // trig to convert local control axis to world
+        float yawradians = pitchyaw.Y * MathF.PI / 180;
         
+        var yawsin = MathF.Sin(yawradians);
+        var yawcos = MathF.Cos(yawradians);
+        
+        Vector2 localmovevec = move.ReadValue<Vector2>();
+        Numerics.Vector2 movevec = new Numerics.Vector2(
+            localmovevec.x * yawcos + localmovevec.y * yawsin,
+            localmovevec.y * yawcos - localmovevec.x * yawsin);
+
+        float verticalspeed = 0.0f;
+        if (jump.WasPressedThisFrame() && IsGrounded()) {
+            verticalspeed = 16.0f;
+        }
+        
+        phys.linearVelocity += new Vector3(movevec.X, verticalspeed, movevec.Y);
+        
+        // get outta here when we escape
         if (pause.WasPressedThisFrame()) {
             Application.Quit();
             
@@ -41,5 +71,9 @@ public class Player : MonoBehaviour {
             UnityEditor.EditorApplication.isPlaying = false;
 #endif
         }
+    }
+    
+    private bool IsGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, collide.bounds.extents.y + 0.1f);
     }
 }
