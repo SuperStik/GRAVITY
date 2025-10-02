3f03bbe0e2f46d9ff062e35ea6ea1b4d3aed3938
SuperStik <daviswood290@gmail.com>
2025-10-02 16:01:43 -0700

    Add camera rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,45 @@
+using System;
+using NUnit.Framework.Constraints;
+using Unity.Mathematics.Geometry;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Numerics = System.Numerics;
 
 public class Player : MonoBehaviour {
     public Camera cam;
     private InputAction look;
+	private InputAction pause;
+
+    private Numerics.Vector2 pitchyaw;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         look = InputSystem.actions.FindAction("Look");
+        pause = InputSystem.actions.FindAction("Pause");
+        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 delta = look.ReadValue<Vector2>();
-        cam.transform.eulerAngles += new Vector3(-delta.y, delta.x);
+        pitchyaw += new Numerics.Vector2(-delta.y, delta.x);
+        // fancy C# switch statement
+        pitchyaw.X = pitchyaw.X switch {
+            >= 90.0f => 90.0f,
+            <= -90.0f => -90.0f,
+            _ => pitchyaw.X
+        };
+
+        cam.transform.eulerAngles = new Vector3(pitchyaw.X, pitchyaw.Y);
+        
+        if (pause.WasPressedThisFrame()) {
+            Application.Quit();
+            
+#if UNITY_EDITOR
+            // thanks Unity
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
     }
 }
